848236d2523bf07a812de6893a071322cb6afa66
github username <github email address>
2018-01-24 10:25:05 -0500

    new api

--- app/models/quickstart.rb
+++ app/models/quickstart.rb
@@ -16,7 +16,7 @@ REDIRECT_URI = 'http://localhost'
 APPLICATION_NAME = 'YouTube Data API Ruby Tests'
 
 # REPLACE WITH NAME/LOCATION OF YOUR client_secrets.json FILE
-CLIENT_SECRETS_PATH = './lib/client_secret.json'
+CLIENT_SECRETS_PATH = './mod-1-final-project/app/models/client_secret.json'
 
 # REPLACE FINAL ARGUMENT WITH FILE WHERE CREDENTIALS WILL BE STORED
 CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
@@ -57,47 +57,48 @@ def print_results(response)
 end
 
 # Build a resource based on a list of properties given as key-value pairs.
-def create_resource(properties)
-  resource = {}
-  properties.each do |prop, value|
-    ref = resource
-    prop_array = prop.to_s.split(".")
-    for p in 0..(prop_array.size - 1)
-      is_array = false
-      key = prop_array[p]
-      # For properties that have array values, convert a name like
-      # "snippet.tags[]" to snippet.tags, but set a flag to handle
-      # the value as an array.
-      if key[-2,2] == "[]"
-        key = key[0...-2]
-        is_array = true
-      end
-      if p == (prop_array.size - 1)
-        if is_array
-          if value == ""
-            ref[key.to_sym] = []
-          else
-            ref[key.to_sym] = value.split(",")
-          end
-        elsif value != ""
-          ref[key.to_sym] = value
-        end
-      elsif ref.include?(key.to_sym)
-        ref = ref[key.to_sym]
-      else
-        ref[key.to_sym] = {}
-        ref = ref[key.to_sym]
-      end
-    end
-  end
-  return resource
-end
+# def create_resource(properties)
+#   resource = {}
+#   properties.each do |prop, value|
+#     ref = resource
+#     prop_array = prop.to_s.split(".")
+#     for p in 0..(prop_array.size - 1)
+#       is_array = false
+#       key = prop_array[p]
+#       # For properties that have array values, convert a name like
+#       # "snippet.tags[]" to snippet.tags, but set a flag to handle
+#       # the value as an array.
+#       if key[-2,2] == "[]"
+#         key = key[0...-2]
+#         is_array = true
+#       end
+#       if p == (prop_array.size - 1)
+#         if is_array
+#           if value == ""
+#             ref[key.to_sym] = []
+#           else
+#             ref[key.to_sym] = value.split(",")
+#           end
+#         elsif value != ""
+#           ref[key.to_sym] = value
+#         end
+#       elsif ref.include?(key.to_sym)
+#         ref = ref[key.to_sym]
+#       else
+#         ref[key.to_sym] = {}
+#         ref = ref[key.to_sym]
+#       end
+#     end
+#   end
+#   return resource
+# end
 
 ### END BOILERPLATE CODE
 
 # Sample ruby code for search.list
-
-def search_list_by_keyword(service, part, **params)
+puts "Input Search:"
+params = {:max_results=>10, :q=>gets.chomp, :type=>"song"}
+def search_list_by_keyword(service, part, params)
   params = params.delete_if { |p, v| v == ''}
   response = service.list_searches(part, params).to_json
   json_response = JSON.parse(response)
@@ -108,20 +109,14 @@ def search_list_by_keyword(service, part, **params)
       no_playlists<<item
     end
   end
-  #print_results(response)
-#   puts ("#{item.fetch("snippet").fetch("title")}. " +
-#   "Its url is https://www.youtube.com/watch?v=#{item.fetch("id").fetch("videoId")}")
+
   no_playlists.each do |item|
     puts ("#{item.fetch("snippet").fetch("title")}. " +
         "Its url is https://www.youtube.com/watch?v=#{item.fetch("id").fetch("videoId")}")
   end
 end
 
-search_list_by_keyword(service, 'snippet',
-  max_results: 10,
-  q: 'Bruno mars',
-  type: 'song')
-
+search_list_by_keyword(service,'snippet', params)
 
 
 
