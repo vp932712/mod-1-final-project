20f51a72b589ed16b809d212beb2cb18631e2911
Vaibhav Patel <vp932712@gmail.com>
2018-01-24 10:21:09 -0500

    one more

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,44 @@
+require "pry"
+
 class User < ActiveRecord::Base
   has_many :user_videos
   has_many :vidoes, through: :user_videos
+
+
+
+  def self.find_user(credentials_input)
+    User.find_by(email: credentials_input)
+
+  end
+
+  def self.create_new_user(email, name)
+    User.create(:email => email, :name =>name)
+
+
+  end
+
+Pry.start
+
+  #
+  # def search(search_input)
+  #
+  #
+  # end
+  #
+  #
+  # def play
+  #
+  # end
+  #
+  #
+  # def like
+  #
+  # end
+  #
+
+
+
+
+
+
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,12 @@
 class Video < ActiveRecord::Base
   has_many :user_videos
   has_many :users, through: :user_videos
+
+  def views(url_string)
+    Video.find_by(url: url_string)
+  end
+
+
+
+
 end
